8b9cd9d7ca9a02d476b618e71ea9b2201eb4368d
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-10-09 08:21:23 -0400

    Add project files.

---
+++ Topic - 7 For loop Programming Assignments/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Topic___7_For_loop_Programming_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assingment 1
+            Console.WriteLine("Press enter to start count down");
+            Console.ReadLine();
+            for (int i = 10; i >= 1; i = i - 1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Blast off");
+            Console.ReadLine();
+
+            //Assignment 2
+            Console.WriteLine("Press enter for some numbers");
+            Console.ReadLine();
+            Console.WriteLine("x      y");
+            Console.WriteLine("_________");
+            for (int i = -10; i <= 10; i = i + 2)
+            {
+                Console.WriteLine(i + "   " + (i * i));
+            }
+            Console.WriteLine("hooray");
+            Console.ReadLine();
+
+            //Assignment 3
+            Console.WriteLine("Enter your name");
+            Console.WriteLine("Name: ");
+            String name = Console.ReadLine();
+            if (name != "Aldworth")
+                for (int i = 1; i <= 9; i = i + 1)
+            {
+                Console.WriteLine(name);
+            }
+            else
+            {
+                for (int i = 1; i <= 4; i = i + 1)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
